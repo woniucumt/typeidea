3ae64a69655dd8730758235c93f0c18eb85aa14e
woniucumt <535633437@qq.com>
2020-06-20 22:56:00 +0800

    修改bug 页面层叠的

--- typeidea/themes/BlogNew/static/js/templatemo-script.js
+++ typeidea/themes/BlogNew/static/js/templatemo-script.js
@@ -5,9 +5,9 @@
 //     // $.backstretch(bgImgs, {duration: 4000, fade: 500});
 // }
 
-const setBg = id => {
-    $.backstretch('show', id);
-}
+// const setBg = id => {
+//     $.backstretch('show', id);
+// }
 // 设置手机适配的歪斜
 const setBgOverlay = () => {
     const windowWidth = window.innerWidth;
@@ -17,11 +17,11 @@ const setBgOverlay = () => {
     $('.tm-bg').height(bgHeight);
 
     if(windowWidth > 768) {
-        tmBgLeft.css('border-left', `0`)
+        tmBgLeft.css('border-left', '0')
                 .css('border-top', `${bgHeight}px solid transparent`);
     } else {
         tmBgLeft.css('border-left', `${windowWidth}px solid transparent`)
-                .css('border-top', `0`);
+                .css('border-top', "0");
     }
 }
 
@@ -29,7 +29,6 @@ const setBgOverlay = () => {
 
 
 $(document).ready(function () {
-    const autoplayBg = false;	// set Auto Play for Background Images
     setBgOverlay();
 
     // const bgControl = $('.tm-bg-control');
@@ -52,25 +51,25 @@ $(document).ready(function () {
     });
     /* Preloader
     * -------------------------------------------------- */
-    var ssPreloader = function() {
-
-        $("html").addClass('ss-preload');
-
-        $WIN.on('load', function() {
-
-            //force page scroll position to top at page refresh
-            // $('html, body').animate({ scrollTop: 0 }, 'normal');
-
-            // will first fade out the loading animation
-            $("#loader").fadeOut("slow", function() {
-                // will fade out the whole DIV that covers the website.
-                $("#preloader").delay(300).fadeOut("slow");
-            });
-
-            // for hero content animations
-            $("html").removeClass('ss-preload');
-            $("html").addClass('ss-loaded');
-
-        });
-    };
+    // var ssPreloader = function() {
+    //
+    //     // $("html").addClass('ss-preload');
+    //
+    //     // $WIN.on('load', function() {
+    //     //
+    //     //     //force page scroll position to top at page refresh
+    //     //     // $('html, body').animate({ scrollTop: 0 }, 'normal');
+    //     //
+    //     //     // will first fade out the loading animation
+    //     //     // $("#loader").fadeOut("slow", function() {
+    //     //     //     // will fade out the whole DIV that covers the website.
+    //     //     //     $("#preloader").delay(300).fadeOut("slow");
+    //     //     // });
+    //     //
+    //     //     // for hero content animations
+    //     //     // $("html").removeClass('ss-preload');
+    //     //     // $("html").addClass('ss-loaded');
+    //     //
+    //     // });
+    // };
 });
